1ee252c00425f8c50a8aaeb6515b9e0b4f97bbc3
RanganaGangadhar <35066304+RanganaGangadhar@users.noreply.github.com>
2018-01-04 13:18:31 +0530

    second commit MainClass

--- src/java/MainClass.java
+++ src/java/MainClass.java
@@ -9,6 +9,8 @@ public class MainClass {
 		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
-		List<String> ggg = Arrays.asList("one","two","three");
+		List<String> ggg = Arrays.asList("one","two","three","four");
+		System.out.println("How are you doing now");
+	    ggg.forEach((n) -> System.out.println("I got"+n));
 	}
 }
