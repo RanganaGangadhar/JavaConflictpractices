1747abc612a18b0b0b425e2e0400f6be88a3a05e
RanganaGangadhar <35066304+RanganaGangadhar@users.noreply.github.com>
2018-01-04 16:37:59 +0530

    testing after creating new branch

--- src/java/MainClass.java
+++ src/java/MainClass.java
@@ -9,8 +9,12 @@ public class MainClass {
 		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
-		List<String> ggg = Arrays.asList("one","two","three","four","five");
-		System.out.println("How are you doing now");
+		List<String> ggg = Arrays.asList("one","two","three","four","five","six","seven");
+		System.out.println("((((((((Testing;;;1)))))");
 	    ggg.forEach((n) -> System.out.println("I got"+n));
+	    
+	    List<Integer> sss = Arrays.asList(12,23,24,35,67);
+	    System.out.println("(((((Testing:::2))))");
+	    sss.forEach((n) -> System.out.println(n));
 	}
 }
