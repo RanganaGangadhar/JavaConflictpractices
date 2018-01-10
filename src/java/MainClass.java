95de69978377347264e06803b41f1bf3445e3ca4
RanganaGangadhar <35066304+RanganaGangadhar@users.noreply.github.com>
2018-01-10 12:20:52 +0530

    commiting form bbb-branch

--- src/java/MainClass.java
+++ src/java/MainClass.java
@@ -10,7 +10,17 @@ public class MainClass {
 	}
 	public static void main(String[] args) {
 		List<String> ggg = Arrays.asList("one","two","three","four","five" );
-		System.out.println("How are you doing now");
+		System.out.println("::::Entering:::First:::loop:::::");
 	    ggg.forEach((n) -> System.out.println("I got"+n));
+	    displayOutput();
 	}
+	private static void displayOutput() {
+		System.out.println("Entering:::Second::::loop");
+		List<Integer> kkk = Arrays.asList(1,2,3,4,5);
+		
+	}
+	
+	
+	
+	
 }
